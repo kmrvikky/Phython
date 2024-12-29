413c857243bf1ccc21772fc151e72d1d19413d05
Kr Vikky <127074056+kmrvikky@users.noreply.github.com>
2024-12-30 00:57:32 +0530

    PracticeSet.py

---
+++ P1.py
@@ -0,0 +1,17 @@
+# print a paragraph/song
+
+print ('''
+O Khaike paan banaras wala
+Khuli jaaye band aqal ka tala
+Phir to aisa kare dhamal
+Seedhi kar de sabki chaal
+O chhora Ganga kinare wala
+O chhora Ganga kinare wala
+Khuli jaaye band aqal ka tala
+''')
+
+print ('''Arre Ram duhaayi!
+Kaise chakkar mein pad gaya
+Haaye haaye haaye
+Kahan jaan phansayi
+''')
---
+++ P4.py
@@ -0,0 +1,6 @@
+# WAP to print the items of a directory using os module.
+
+import os
+directory_path = 'D:\Python_CWH\PS'
+items = os.listdir(directory_path)
+print(items)
